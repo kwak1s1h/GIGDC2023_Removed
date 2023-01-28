8d829786178a7c5470fb1e6e5558bb4f7ad7a351
ahslxjvhapt12 <likjhee1@gmail.com>
2023-01-29 04:42:20 +0900

    PlayerMove
    JoyStick
    -ㅇㅣㅅㅏㅇㅎㅐ

---
+++ ValhallaProject/Assets/01_Script/JaeHee/PlayerMove.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Timers;
+using UnityEditor.U2D;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    [SerializeField] private JoyStick joyStick;
+
+    private Rigidbody2D rigid = null;
+
+
+    private void Awake()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        rigid.velocity = new Vector2(joyStick.Horizontal, joyStick.Vertical);
+        //if (joyStick.Horizontal != 0 || joyStick.Vertical != 0)
+        //{
+        //    Move();
+        //}
+    }
+
+    private void Move()
+    {
+        //Vector3 hMovement = Vector3.right * speed * Time.fixedDeltaTime * joyStick.Horizontal;
+        //Vector3 vMovement = Vector3.up * speed * Time.fixedDeltaTime * joyStick.Vertical;
+
+    }
+}
