1474f728d02767fd516380a6973e85cdbbd02eb7
ahslxjvhapt12 <likjhee1@gmail.com>
2023-02-18 00:34:29 +0900

    DashFix

--- ValhallaProject/Assets/01_Script/JaeHee/PlayerController.cs
+++ ValhallaProject/Assets/01_Script/JaeHee/PlayerController.cs
@@ -1,12 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private JoyStick joyStick;
-    [Range(1, 100)][SerializeField] private float dashPower = 1;
+    [Range(1, 30)][SerializeField] private float dashPower = 1;
     //플레이어 우짜노 ㅋㅋ
+    [SerializeField] [Range(0, 1)] private float dashDuration;
+    [SerializeField] [Range(0, 1)] private float dashCoolTime;
+    private bool canDash = true;
 
     private Rigidbody2D _rigid;
 
@@ -17,6 +21,17 @@ public class PlayerController : MonoBehaviour
 
     public void Dash()
     {
+        if (canDash == false) return;
+        canDash = false;
         _rigid.AddForce(joyStick.InputVector * dashPower, ForceMode2D.Impulse);
+        StartCoroutine(DashStop());
+    }
+
+    private IEnumerator DashStop()
+    {
+        yield return new WaitForSeconds(dashDuration);
+        _rigid.velocity = Vector3.zero;
+        yield return new WaitForSeconds(dashCoolTime);
+        canDash = true;
     }
 }
